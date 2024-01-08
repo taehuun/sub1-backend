581845adf19d1764d80aad352f90f1905be57703
서지원 <sjs3326@naver.com>
2024-01-08 15:18:08 +0900

    chore: 스프링 프로젝트 생성

---
+++ src/test/java/com/ssafy/sub1/Sub1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.sub1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sub1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
